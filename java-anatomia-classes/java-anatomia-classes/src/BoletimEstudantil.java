2ac40c5cfd40c6ded56d1a139bdc12043ddd8795
Thalita Meira <thalita_meira@hotmail.com>
2023-09-13 15:16:53 -0300

    adiciona classe BoletimEstudantil

---
+++ java-anatomia-classes/java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+        
+        int mediaFinal = 6;
+
+        if(mediaFinal <6)
+        System.out.print("REPROVADO");
+        else if(mediaFinal == 6)
+        System.out.print("PROVA MINERVA");
+        else
+        System.out.print("APROVADO");
+    }
+    
+}
