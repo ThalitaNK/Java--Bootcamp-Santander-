20663e48e63935e5649e2ae593adbc2738b846c8
Thalita Meira <thalita_meira@hotmail.com>
2023-09-17 15:02:53 -0300

    operadores ternários

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -16,5 +16,33 @@ public class Operadores {
         
         System.out.println(numero);
 
+        //x repetição
+
+        int numero2 = 5;
+
+        numero2++;
+
+        //numero2 = numero + 1;
+
+        System.out.println(numero2);
+
+        //operador ternário
+        int a,b;
+
+        a = 5;
+        b = 6;
+    
+        //String resultado = "";
+        // if(a==b)
+        //     resultado =  "verdadeiro";
+        // else
+        //     resultado = "falso";
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+
+
     }
 }
