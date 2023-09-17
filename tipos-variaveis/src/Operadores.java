1594d655e7c2b470dda0ac75c690b2a1e2f64d82
Thalita Meira <thalita_meira@hotmail.com>
2023-09-17 14:34:51 -0300

    imprime string nomeCompleto

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,4 +1,10 @@
 public class Operadores {
+
+    public static void main(String[] args) {
     
     String nomeCompleto = "Thalita" + "Meira";
+
+    System.out.println (nomeCompleto);
+
+    }
 }
