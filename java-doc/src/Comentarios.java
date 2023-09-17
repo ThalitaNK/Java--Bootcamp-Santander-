1dce567a503cadc06c5239ce569d9ce303efa6dc
Thalita Meira <thalita_meira@hotmail.com>
2023-09-17 19:25:47 -0300

    atualiza classe comentários

--- java-doc/src/Comentarios.java
+++ java-doc/src/Comentarios.java
@@ -16,6 +16,23 @@ public class Comentarios {
 
     }
 
+    //O QUE NÃO FAZER: ADOTAR O USO DE COMENTÁRIOS NÃO EXCLUI A NECESSIDADE DO USO DAS BOAS PRÁTICAS NA HORA DE CODAR!!!
+    /*
+ * Este método foi elaborado as pressas
+ * por isso eu abreviei o nome das variáveis
+ * mas olha, ele tem a finalidade de somar ou  multiplicar
+ * dois números
+ */
+    public int somaMultiplica (int n, int x, String m){
+    int r = 0; // r é igual ao resultado
+    if (m == "M"){ // M= multiplicação
+        r= n * x;
+    }else{
+        // se não soma mesmo
+        r = n + x;
+    }
+    return r;
+}
 
     
 }
