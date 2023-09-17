7e94e90358123caafee2850cb1bcfc0f9f5963ca
Thalita Meira <thalita_meira@hotmail.com>
2023-09-17 19:06:27 -0300

    cria classe para estudo de como usar os comentários da melhor forma

---
+++ java-doc/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+
+    // comentário única linha
+
+    /*comentário 
+     * multi
+     * linha
+     */
+
+     /**Duas estrelinhas para documentar
+      * metodos
+      *com facilidade
+      */
+
+    public void metodo(){
+
+    }
+
+
+    
+}
