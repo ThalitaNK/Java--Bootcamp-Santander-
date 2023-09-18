93c2e944221cd88aef9549a03039810fbaaebe44
Thalita Meira <thalita_meira@hotmail.com>
2023-09-18 15:49:27 -0300

    ajuste condicional saldo

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -5,7 +5,7 @@ public class CaixaEletronico {
 
         if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
-            System.out.println("Novo saldo:" + saldo);
+            System.out.println("Novo saldo: " + saldo);
         }else
             System.out.println("Saldo insuficiente.");
 
