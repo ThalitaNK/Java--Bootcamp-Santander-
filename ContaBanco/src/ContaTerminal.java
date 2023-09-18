80086fd6bb7d13703effa8cbce0753e3c16ff5d6
Thalita Meira <thalita_meira@hotmail.com>
2023-09-18 14:05:34 -0300

    desenvolve projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+/**
+ * @author
+ */
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        int numero = 1021;
-        String agencia = "067-8";
-        String nomeCliente = "MARIO ANDRADE";
-        double saldo = 237.48;
 
+        // Cria um objeto Scanner para ler dados do terminal
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita ao usuário que insira o número da Agência
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        // Solicita ao usuário que insira o número da Conta
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha deixada pelo nextInt()
+
+        // Solicita ao usuário que insira o nome do cliente
+        System.out.print("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        // Solicita ao usuário que insira o saldo
+        System.out.print("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        // Fecha o scanner
+        scanner.close();
+
+        // Exibe a mensagem com os dados fornecidos pelo usuário
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");067-8
 
     }
+
 }
