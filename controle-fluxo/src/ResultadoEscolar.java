92f90753614ba19d6df36e89e07f465a4004a334
Thalita Meira <thalita_meira@hotmail.com>
2023-09-18 16:16:33 -0300

    estudo condição ternária

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -4,11 +4,14 @@ public class ResultadoEscolar {
 
         int nota = 5;
 
-        if (nota >= 7)
-            System.out.println("Aprovado");
-        else if(nota > 5 && nota < 7)
-            System.out.println("Em recuperação.");
-        else
-            System.out.println("Reprovado");
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+        // if (nota >= 7)
+        //     System.out.println("Aprovado");
+        // else if(nota > 5 && nota < 7)
+        //     System.out.println("Em recuperação.");
+        // else
+        //     System.out.println("Reprovado");
     }
 }
