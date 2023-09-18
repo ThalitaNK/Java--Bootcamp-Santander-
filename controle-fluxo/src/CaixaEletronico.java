70341b7bfd4d1a0b86a8fa880e0e4128caca9b1c
Thalita Meira <thalita_meira@hotmail.com>
2023-09-18 15:48:25 -0300

    estudo sobre condicionais compostas

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,10 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 30;
 
-        if (valorSolicitado < saldo)
+        if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo:" + saldo);
+        }else
+            System.out.println("Saldo insuficiente.");
 
         System.out.println(saldo);
     }
