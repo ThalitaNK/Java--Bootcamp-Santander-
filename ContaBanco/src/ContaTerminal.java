726bd27134458ba1877cd37262e7e89c0a23a18e
Thalita Meira <thalita_meira@hotmail.com>
2023-09-18 13:50:53 -0300

    first commit

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        int numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "MARIO ANDRADE";
+        double saldo = 237.48;
+
+
+    }
+}
