1a3d6470a45cfbcf84c57c31681504afcac33469
Thalita Meira <thalita_meira@hotmail.com>
2023-09-21 10:34:31 -0300

    estudo for com array

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,14 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+        // ExemploFor.java
+
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+
+    }
+
+}
