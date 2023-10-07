4d019052d02c38bce01bda86135a31a013a11e91
Thalita Meira <thalita_meira@hotmail.com>
2023-10-07 20:28:28 -0300

    imprime lista com selecionados

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -3,7 +3,25 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
 
-        selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados(){
+        String[] candidatos = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento: ");
+
+        for(int indice=0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de nº " + (indice+1) + " é o " + candidatos[indice]);
+
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+
+        for(String candidato:candidatos){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
+
     }
 
     static void selecaoCandidatos() {
