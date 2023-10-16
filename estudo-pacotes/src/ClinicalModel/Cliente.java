a49778e30bdf81eb404699603944337b301b6d77
Thalita Meira <thalita_meira@hotmail.com>
2023-10-16 17:34:39 -0300

    Organiza pacotes

---
+++ estudo-pacotes/src/ClinicalModel/Cliente.java
@@ -0,0 +1,5 @@
+package ClinicalModel;
+
+public class Cliente {
+    
+}
---
+++ estudo-pacotes/src/ClinicalService/ExameService.java
@@ -0,0 +1,7 @@
+package ClinicalService;
+
+import ClinicalModel.Exame;
+
+public class ExameService {
+    void salvarExame(Exame Exame){}
+}
